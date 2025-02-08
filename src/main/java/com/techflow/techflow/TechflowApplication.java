5b67f4478a904e27ffc49f2eaa6a442f9d6b40ba
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-02-08 13:27:11 +0100

    init

---
+++ src/main/java/com/techflow/techflow/TechflowApplication.java
@@ -0,0 +1,13 @@
+package com.techflow.techflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techflow/techflow/TechflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techflow.techflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
